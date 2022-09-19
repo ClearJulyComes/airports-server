300189a98cd62b6a24529ab1141243bfc589fa8e
Viktor Domogashev <vdomogashev@aligntech.com>
2022-09-20 01:35:36 +0700

    Configure mongodb

--- src/main/java/com/example/airportsapi/dao/AirportRepository.java
+++ src/main/java/com/example/airportsapi/dao/AirportRepository.java
@@ -1,7 +1,10 @@
 package com.example.airportsapi.dao;
 
 import com.example.airportsapi.model.Airport;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
-public interface AirportRepository {
+public interface AirportRepository extends MongoRepository<Airport, String> {
+    @Query("{id: '?0'}")
     Airport findAirport(int id);
 }
--- src/main/java/com/example/airportsapi/model/Airport.java
+++ src/main/java/com/example/airportsapi/model/Airport.java
@@ -1,9 +1,13 @@
 package com.example.airportsapi.model;
 
 import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Value
+@Document(collection = "airports")
 public class Airport {
+    @Id
     int id;
     String data;
 }
